df9b5d575cfed353240d041a2b415dd1ec637843
arnoldpaye <arnoldpayet@gmail.com>
2015-05-13 12:21:36 -0400

    add evento de aplicacion cambiodeestacion

--- app/view/InformacionDeCancion.js
+++ app/view/InformacionDeCancion.js
@@ -1,5 +1,10 @@
 Ext.define('Zunk.view.InformacionDeCancion', {
   extend: 'Ext.panel.Panel',
   alias: 'widget.informaciondecancion',
-  tpl: 'Informacion {nombre}'
+  tpl: 'Informacion {nombre}',
+
+  initComponent: function() {
+    this.update({});
+    this.callParent();
+  }
 });
